16e998974d3a6e73ad42845c54f5fecb69aa09de
YC-L <game0427game@gmail.com>
2021-06-14 16:43:41 +0100

    main

---
+++ pyoffice_install_deb.sh
@@ -0,0 +1,29 @@
+sudo apt install python3.9
+
+python3.9 -m pip install py-office
+
+cd ~/.local/share/applications
+
+bash -c "echo '[Desktop Entry]
+Name=py-office-sheet
+Version=1.0
+GenericName=spreadsheet
+Keywords=python;
+TryExec=py-office-sheet
+Exec=py-office-sheet
+Icon=pyoffice.png
+Terminal=false
+Categories=Office;
+Type=Application' > py-office-sheet.desktop"
+
+bash -c "echo '[Desktop Entry]
+Name=py-office-learn
+Version=1.0
+GenericName=machine learning
+Keywords=python;
+TryExec=py-office-learn
+Exec=py-office-sheet
+Icon=pyoffice.png
+Terminal=false
+Categories=Office;
+Type=Application' > py-office-learn.desktop"
